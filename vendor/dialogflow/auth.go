0882fa3daee8b0d5d49b5b60dfa61f6aaaf81e40
Lucas Ansei <lucas.ansei@appcivico.com>
2021-08-03 15:21:30 -0300

    Adding vendor folder

---
+++ vendor/dialogflow/auth.go
@@ -0,0 +1,48 @@
+package auth
+
+// [START import_libraries]
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"cloud.google.com/go/storage"
+	cloudkms "google.golang.org/api/cloudkms/v1"
+	"google.golang.org/api/iterator"
+)
+
+// [END import_libraries]
+
+func implicit() {
+	ctx := context.Background()
+
+	// For API packages whose import path is starting with "cloud.google.com/go",
+	// such as cloud.google.com/go/storage in this case, if there are no credentials
+	// provided, the client library will look for credentials in the environment.
+	storageClient, err := storage.NewClient(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer storageClient.Close()
+
+	it := storageClient.Buckets(ctx, "project-id")
+	for {
+		bucketAttrs, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(bucketAttrs.Name)
+	}
+
+	// For packages whose import path is starting with "google.golang.org/api",
+	// such as google.golang.org/api/cloudkms/v1, use NewService to create the client.
+	kmsService, err := cloudkms.NewService(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_ = kmsService
+}
