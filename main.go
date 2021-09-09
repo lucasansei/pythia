f2d5d161d8325268883dcf12562ccf82551ef839
lucasansei <lucas.ansei@appcivico.com>
2021-09-09 17:03:25 -0300

    Adding error treatment

--- main.go
+++ main.go
@@ -30,9 +30,32 @@ func main() {
 	router.GET("/text", func(c *gin.Context) {
 		text := c.Query("text")
 
+		if len(text) <= 1 {
+			c.JSON(400, gin.H{
+				"message": "?text= is too small",
+				"error":   "invalid_text",
+			})
+			return
+		}
+		if len(text) > 1000 {
+			c.JSON(400, gin.H{
+				"message": "?text= is too big",
+				"error":   "invalid_text",
+			})
+			return
+		}
+
 		res, err := dialogflow.DetectIntentText(os.Getenv("GOOGLE_CLOUD_PROJECT_NAME"), "web_search_session", text, "pt-BR")
+		if err != nil {
+			fmt.Println(err)
+			fmt.Printf("Failed to detect intent %s", err.Error())
+
+			c.JSON(500, gin.H{
+				"error": "failed to process request.",
+			})
+			return
+		}
 		fmt.Println(res)
-		fmt.Println(err)
 
 		c.JSON(200, gin.H{
 			"dialogflow_result": res,
@@ -40,12 +63,29 @@ func main() {
 	})
 
 	router.POST("/audio", func(c *gin.Context) {
-		file, _ := c.FormFile("audio")
+		file, err := c.FormFile("audio")
+		if err != nil {
+			fmt.Printf("Failed to get FormFile %s", err.Error())
+			c.JSON(400, gin.H{
+				"message": "Não conseguimos abrir o arquivo de audio",
+				"error":   "invalid_file",
+			})
+			return
+		}
+		if file == nil {
+			fmt.Printf("Missing FormFile")
+			c.JSON(400, gin.H{
+				"message": "Não conseguimos encontrar o arquivo de audio",
+				"error":   "missing_file",
+			})
+			return
+		}
 
 		tmpExt := path.Ext(file.Filename)
 		if tmpExt == "" {
 			c.JSON(400, gin.H{
-				"error": "invalid file name: missing file extension",
+				"message": "Não conseguimos encontrar a extensão do arquivo de audio",
+				"error":   "invalid_file_ext",
 			})
 			return
 		}
@@ -67,7 +107,8 @@ func main() {
 			fmt.Printf("Failed to detect intent %s", err.Error())
 
 			c.JSON(500, gin.H{
-				"error": "failed to process request.",
+				"message": "Aconteceu um erro ao consultar o serviço DialogFlow",
+				"error":   "dialog_flow_error",
 			})
 			return
 		}
